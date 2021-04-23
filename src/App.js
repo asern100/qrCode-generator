911bad5a493c6321f4cb989eb57899aba769e37f
Ahmed <saiefeddina@gmail.com>
2021-04-23 14:05:15 +0100

    up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import { QRCode } from 'react-qrcode-logo';
+import {Container,Row , Col,} from 'reactstrap'
 
 function App() {
+  const [array, setarray] = useState([])
+  const [sarray, setsarray] = useState([])
+
+ const generate = (array) => {
+   var x=0;
+   
+  for (let index = 5; index < 20; index++) { 
+    
+    if((index % 5) == 0) {array.push(` ######################## \n ####### Futur Home ####### \n ######################## \n ######## prix : { ${index.toString()} } ####### \n ########################`)}
+    x+=1
+  }
+ }
+ const sgenerate = (array) => {
+  var x=0;
+  
+ for (let index = 5; index < 20; index++) { 
+   
+   if((index % 5) == 0) {array.push(`  Prix : { ${index.toString()} }`)}
+   x+=1
+ }
+}
+ generate(array)
+ sgenerate(sarray)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    
+    <Row>
+    {
+      
+      console.log(array)
+    }
+    {array.map(e => 
+      <Col><Row>
+      <QRCode size={250} value={e} logoImage="https://p.ventesprivees-fr.com/future-home.png" logoWidth={50} logoHeight={50}/>
+      </Row>
+      </Col>
+      )}
+   </Row>
+   <Row>
+   {
+     
+     console.log(sarray)
+   }
+   {sarray.map(e => 
+     <Col><Row>
+     <QRCode size={50} value={e} logoImage="https://p.ventesprivees-fr.com/future-home.png" logoWidth={10} logoHeight={10}/>
+     </Row>
+     </Col>
+     )}
+  </Row>
     </div>
   );
 }
